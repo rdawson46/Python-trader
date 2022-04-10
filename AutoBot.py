38a4b59b4f1e96c0dbc7f8bf9673862c89362337
Ryan Crooks <77314355+rdawson46@users.noreply.github.com>
2022-04-10 14:02:00 -0400

    Add files via upload

---
+++ AutoBot.py
@@ -0,0 +1,125 @@
+import alpaca_trade_api as tradeapi
+
+
+class StockBot(object):
+    def __init__(self):
+        self.key = 'PK35MDGAXOIF0NSIZ8CC'
+        self.secret = 'jnfAgOZRKbaAEMe0cdOW8nyjUtwraBBBFBjS19cs'
+        self.url = 'https://paper-api.alpaca.markets'
+        self.api = tradeapi.REST(self.key, self.secret, self.url)
+        self.symbols = ['CROX', 'VOO']
+        self.current_order = None
+        self.last_price = 1
+        self.positions = []
+        self.risk = 0
+
+        for symbol in self.symbols:
+            try:
+                self.positions.append(int(self.api.get_position(symbol).qty))
+            except:
+                self.positions.append(0)
+
+    def get_weekly_average(self):
+        averages = {}
+        barset = self.api.get_barset(self.symbols, 'day', limit=5)
+
+        for symbol in self.symbols:
+            stock_barset = barset[symbol]
+            total = 0
+
+            for day in stock_barset:
+                total += (day.h + day.l) / 2
+
+            averages[symbol] = total / 5
+
+        return averages
+
+    def get_two_week_average(self):
+        averages = {}
+        barset = self.api.get_barset(self.symbols, 'day', limit=10)
+
+        for symbol in self.symbols:
+            stock_barset = barset[symbol]
+            total = 0
+
+            for day in stock_barset:
+                total += (day.h + day.l) / 2
+
+            averages[symbol] = total / 10
+
+        return averages
+
+    def get_monthly_average(self):
+        averages = {}
+        barset = self.api.get_barset(self.symbols, 'day', limit=21)
+
+        for symbol in self.symbols:
+            stock_barset = barset[symbol]
+            total = 0
+
+            for day in stock_barset:
+                total += (day.h + day.l) / 2
+
+            averages[symbol] = total / 21
+
+        return averages
+
+    def get_yearly_average(self):
+        averages = {}
+        barset = self.api.get_barset(self.symbols, 'day', limit=252)
+
+        for symbol in self.symbols:
+            stock_barset = barset[symbol]
+            total = 0
+
+            for day in stock_barset:
+                total += (day.h + day.l) / 2
+
+            averages[symbol] = total / 252
+
+        return averages
+
+    def buy(self, symbol):
+        if self.current_order is not None:
+            self.api.cancel_order(self.current_order.id)
+
+        # figure out how to define buy_quantity and figure out what to so about last price
+        buy_quantity = 3
+        self.current_order = self.api.submit_order(symbol, buy_quantity, 'buy', 'limit', 'day', self.last_price)
+        print(f'Purchasing {buy_quantity} shares of {symbol}')
+
+    def sell(self, symbol):
+        if self.current_order is not None:
+            self.api.cancel_order(self.current_order.id)
+
+        # figure out how to define sell_quantity and figure out what to so about last price
+        sell_quantity = 0
+        self.current_order = self.api.submit_order(symbol, sell_quantity, 'sell', 'limit', 'day', self.last_price)
+        print(f'Selling {sell_quantity} shares of {symbol}')
+
+    @property
+    def get_available_balance(self):
+        return self.api.get_account().buying_power
+
+    @property
+    def get_total(self):
+        return self.api.get_account()
+
+    @property
+    def is_open(self):
+        return self.api.get_clock().is_open
+
+    def algorithm(self):
+        # figure out what to do here
+        # strategy 1- figure out how to rid the averages and make decisions based of that
+
+        # need to find a way to calculate how much will be bought or sold
+        for symbol in self.symbols:
+            ...
+
+        pass
+
+
+if __name__ == '__main__':
+    bot = StockBot()
+    print(bot.api.get_barset(bot.symbols[0], 'day', limit=1))
---
+++ tradingInterface.py
@@ -0,0 +1,93 @@
+import alpaca_trade_api as alpaca
+
+
+class Trader(object):
+    def __init__(self):
+        self.key = 'PK35MDGAXOIF0NSIZ8CC'
+        self.secret = 'jnfAgOZRKbaAEMe0cdOW8nyjUtwraBBBFBjS19cs'
+        self.url = 'https://paper-api.alpaca.markets'
+        self.api = alpaca.REST(self.key, self.secret, self.url)
+        self.symbols = ['CROX', 'VOO']
+        self.current_order = None
+        self.last_price = 1
+        self.positions = []
+
+        for symbol in self.symbols:
+            try:
+                self.positions.append(int(self.api.get_position(symbol).qty))
+            except:
+                self.positions.append(0)
+
+    def interaction_menu(self):
+        print("-------Trading Interface-------")
+        for i in range(len(self.symbols)):
+            print(f'{i+1}. {self.symbols[i]}')
+
+        print(f'{len(self.symbols) + 1}. Check balance')
+        print(f'{len(self.symbols) + 2}. Check Positions')
+
+        choice = int(input("Select an Option: "))
+
+        if choice > len(self.symbols) + 2:
+            choice = int(input("Select a valid option:  "))
+
+        if choice == len(self.symbols) + 1:
+            # calls method to check the total balance
+            print(int(self.api.get_account().portfolio_value) - int(self.api.get_account().cash))
+
+        elif choice == len(self.symbols) + 2:
+            # calls method to check positions for every stock
+            for symbol in self.symbols:
+                try:
+                    print(f'{symbol}) {self.api.get_position(symbol)} shares')
+                except:
+                    print(f'{symbol}) No position found')
+
+        else:
+            stock = self.symbols[choice - 1]
+            print(f"\nOptions for {stock}")
+            print("1. Buy\n2. Sell")
+
+            choice = 0
+            while not 1 <= choice <= 2:
+                choice = int(input("Enter Option Number: "))
+
+            if choice == 1:
+                # calls method to buy chosen stock
+                self.buy(stock)
+
+            elif choice == 2:
+                # calls method to sell chosen stock
+                self.sell(stock)
+
+    def buy(self, symbol):
+        if self.current_order is not None:
+            self.api.cancel_order(self.current_order.id)
+
+        buy_quantity = 0
+
+        # add check to make sure the user can afford to buy that number of shares
+        while buy_quantity == 0:
+            buy_quantity = int(input("How many shares would you like to buy: "))
+
+        self.current_order = self.api.submit_order(symbol, buy_quantity, 'buy', 'market', 'day')
+        print(f'Buying {buy_quantity} shares of {symbol}')
+
+    def sell(self, symbol):
+        if self.current_order is not None:
+            self.api.cancel_order(self.current_order.id)
+
+        sell_quantity = 0
+
+        # add a check to make sure that the user has that many shares
+        while sell_quantity == 0:
+            sell_quantity = int(input("How many shares would you like to sell: "))
+
+        self.current_order = self.api.submit_order(symbol, sell_quantity, "sell", 'market', 'day')
+        print(f'Selling {sell_quantity} shares of {symbol}')
+
+
+if __name__ == "__main__":
+    x = Trader()
+    x.interaction_menu()
+
